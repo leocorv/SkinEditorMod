d6fccbcd599b7c228d2dabb08bbd67ab33a4e933
Leo Corv / Nerix <nerix422@gmail.com>
2026-09-17 17:12:32 +0200

    fix: release SkinEditor client resources on disconnect

--- src/client/java/org/nerix/skineditor/client/SkinEditorModClient.java
+++ src/client/java/org/nerix/skineditor/client/SkinEditorModClient.java
@@ -3,30 +3,41 @@ package org.nerix.skineditor.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.SkinTextures;
 import net.minecraft.util.Identifier;
 
 public class SkinEditorModClient implements ClientModInitializer {
 
-    // On stocke l'ID pour le Mixin
     public static Identifier TEMP_SKIN_ID = null;
     public static SkinTextures.Model TEMP_SKIN_MODEL = SkinTextures.Model.WIDE;
-
-    // NOUVEAU : On stocke l'objet Skin entier pour ne pas qu'il soit supprimé par le Garbage Collector
     public static EditableSkin PERMANENT_SKIN = null;
 
     @Override
     public void onInitializeClient() {
         ClientNetworkHandler.registerClient();
+
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             dispatcher.register(ClientCommandManager.literal("skineditor")
                     .executes(context -> {
-                        MinecraftClient.getInstance().send(() -> {
-                            MinecraftClient.getInstance().setScreen(new SkinEditorScreen());
-                        });
+                        MinecraftClient.getInstance().send(() ->
+                                MinecraftClient.getInstance().setScreen(new SkinEditorScreen()));
                         return 1;
                     }));
         });
+
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> clearSessionState());
+    }
+
+    private static void clearSessionState() {
+        if (PERMANENT_SKIN != null) {
+            PERMANENT_SKIN.close();
+            PERMANENT_SKIN = null;
+        }
+
+        TEMP_SKIN_ID = null;
+        TEMP_SKIN_MODEL = SkinTextures.Model.WIDE;
+        ClientNetworkHandler.CLIENT_ROOT_CATEGORY = null;
     }
 }
